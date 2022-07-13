15e2c457a3c9bac3022eafc2d06c3825abc460bc
Debo <deborahkollman@gmail.com>
2022-07-13 17:03:10 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import ScrollToTop from './components/ScrollTop';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar id="back-to-top-anchor" />
+      <ScrollToTop/>
+    </React.Fragment>
+    // <div className="App">
+    //   <NavBar/>
+    //   <div className='home'>
+    //     <h1>Hola! Soy Deborah</h1>
+    //     <h2>Full Stack Web Developer</h2>
+    //     <h3>About</h3>
+    //   </div>
+    //   <div className='proyects'>
+    //     <p>Aca va un carrusel</p>
+    //   </div>
+    //   <div className='skills'>
+    //     <p>Muchos iconos(con links)</p>
+    //   </div>
+    //   <div className='contact'>
+    //     <p>Telefono</p>
+    //     <p>LinkedIn</p>
+    //     <p>Github</p>
+    //   </div>
+    // </div>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+import Divider from '@mui/material/Divider';
+import './styles/NavBar.scss'
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+
+export default function NavBar(props) {
+  return (
+    <React.Fragment>
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar className='nav-bar'>
+            <div>
+                <div>Home</div>
+                <Divider orientation="vertical" variant="middle" flexItem />
+                <div>Proyectos</div>
+                <Divider orientation="vertical" variant="middle" flexItem />
+                <div>Habilidades</div>
+                <Divider orientation="vertical" variant="middle" flexItem />
+                <div>Contacto</div>
+            </div>
+            <div>
+                <div>Light Dark mode</div>
+                <Divider orientation="vertical" flexItem />
+                <div>Lenguaje</div>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+    </React.Fragment>
+  );
+}
